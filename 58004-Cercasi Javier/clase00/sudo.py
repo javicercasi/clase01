bdc227ff0a501bcac90b183e7320fd87f4ca5bca
javicercasi <jacercasii@gmail.com>
2019-10-09 13:11:03 -0300

    comit local

--- 58004-Cercasi Javier/clase00/sudo.py
+++ 58004-Cercasi Javier/clase00/sudo.py
@@ -1,28 +1,86 @@
 import copy
 class Sudoku():
 
-    
-    def __init__(self):
+    def __init__(self, tabla):
         self.is_playing = True
-        self.matriz = []
-        self.fijos = []
+        self.matriz = [ [ 0 for __ in range(9) ] for _ in range(9) ]
+        self.fijos = [ [ 0 for __ in range(9) ] for _ in range(9) ]
+        self.string_converter(tabla)
 
-        
     def string_converter(self,tabla):
         #Este metodo chequeara las posiciones de los valores fijos
         large=len(tabla)
-        for i in range (large):
-            self.matriz.append(list(tabla[i]))
-        print(matriz)
-   
-        for i in range (9):
+        for i in range (len(tabla)):
             for j in range (9):
-                if  self.matriz [i][j] == "x":
-                    self.fijos.append
+                self.matriz[i][j]=tabla[i][j]
+                self.fijos[i][j]=tabla[i][j] 
 
-    def chequeo
+#Chequearemos que si el valor de mi matriz es distinto de x, es porque 
+# Posee un valor fijo de fabrica, me devuelve un true 
+    def valores_fijos(self,m,n):
+        #print(self.fijos[m][n])
+        return self.fijos[m][n] == "x"
 
-    '''
+    def repeticion_fila_columna(self,fila,columna,valor):
+        #Chequearemos las columnas, dejando cada fila fija
+        
+        for i in range(9):
+            if self.matriz[i][columna]== valor:
+                return (False)
+
+            #Chequearemos las filas, dejando cada columna fija
+            if self.matriz[fila][i]== valor:
+                return False
+            
+        return True
+
+    def repeticion_zona(self, fila, columna, valor):
+        if (fila < 3):
+            fila = 0
+        if (fila >= 3 and fila < 6):
+            fila = 3
+        else:
+            fila = 6
+        
+        if (columna < 3):
+            columna = 0
+        if (columna >= 3 and columna < 6):
+            columna = 3
+        else:
+            columna = 6
+
+        for i in range(3):
+            for j in range(3):
+                if (self.matriz[fila + i][columna + j] == valor):
+                    return False
+                else:
+                    return True
+
+    def general(self, fila, columna, valor):
+        paso = 0
+
+        if self.valores_fijos(fila, columna) is False:
+            print("Valor fijado de fabrica")
+        else:
+            paso += 1
+
+        if self.repeticion_fila_columna(fila, columna,valor) is False:
+            print("Valor repetido en fila y/o columna")
+        else:
+            paso += 1
+ 
+        if self.repeticion_zona(fila, columna, valor) is False:
+            print("Valor repetido en el bloque")
+        else:
+            paso += 1
+        if paso == 3:
+            return True
+        else:
+            return False
+        
+
+
+   
     
 
 
--- 58004-Cercasi Javier/clase00/test.py
+++ 58004-Cercasi Javier/clase00/test.py
@@ -4,31 +4,38 @@ from sudo import Sudoku
 
 
 class TestSudoku(unittest.TestCase):
-    def Setup(self):
-        game= Sudoku()
-        game.string_converter = (["53xx7xxxx",
-                         "6xx195xxx",
-                         "x98xxxx6x",
-                         "8xxx6xxx3",
-                         "4xx8x3xx1",
-                         "7xxx2xxx6",
-                         "x6xxxx28x",
-                         "x3x419xx5",
-                         "xxxx8xx79"])
+    def setUp(self):
+        self.game= Sudoku(["53xx7xxxx",
+                           "6xx195xxx",
+                           "x98xxxx6x",
+                           "8xxx6xxx3",
+                           "4xx8x3xx1",
+                           "7xxx2xxx6",
+                           "x6xxxx28x",
+                           "x3x419xx5",
+                           "xxxx8xx79"])
+        
 
-        self.assertFalse(Sudoku.is_playing) 
-        print (matriz)
-                
-    
-    """def test_play_incorrecto(self):
-        result = self.game.panel_numero(1, 1, 3)
-        self.assertFalse(self.game.poner_numero(1, 1, 3), 'El lugar esta ocupado')
-        self.assertTrue(self.game.is_playing)
+        self.assertTrue(self.game.is_playing) 
+    def test_error(self):
 
-    def test_play_correcto(self):
-        result = self.game.panel_numero(3, 1, 3)
-        self.assertFalse(self.game.poner_numero(3, 1, 3), result)
-        self.assertTrue(self.game.is_playing)
-    """
+        self.assertFalse(self.game.valores_fijos(0,0))
+        self.assertFalse(self.game.repeticion_fila_columna(0,2,'7'))
+        self.assertFalse(self.game.repeticion_zona(8,6,'2'))
+        self.assertFalse(self.game.general(8,6,'8'))
+        self.assertFalse(self.game.general(0,0,'8'))
+
+    def test_error(self):
+        
+
+    def test_valor_disponible(self):
+
+        self.assertTrue(self.game.valores_fijos(1,1))
+        self.assertTrue(self.game.repeticion_fila_columna(0,8,'4'))
+        self.assertTrue(self.game.repeticion_zona(8,1,'1'))
+        self.assertTrue(self.game.repeticion_zona(3,7,'7'))
+        
+
+
 if __name__ == '__main__':
     unittest.main()
